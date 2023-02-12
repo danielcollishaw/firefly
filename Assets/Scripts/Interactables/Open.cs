5c9e77f2cc59e584d58e6f40cceb091ca63dd908
danielcollishaw <cubita98@me.com>
2023-02-12 17:07:47 -0500

    Got basic pressure plate, needs mechanics to implement further

---
+++ Assets/Scripts/Interactables/Open.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateControl : MonoBehaviour
+{
+    public void OpenGate()
+    {
+        gameObject.SetActive(false);
+    }
+}
