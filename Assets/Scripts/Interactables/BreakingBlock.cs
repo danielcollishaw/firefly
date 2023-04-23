8a056c55cd8df33d4ad97ae415885077a320efbc
Justin <contact@atlas-applications.com>
2023-04-23 15:57:28 -0400

    Breaking Block Updates
    Progress...

--- Assets/Scripts/Interactables/BreakingBlock.cs
+++ Assets/Scripts/Interactables/BreakingBlock.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class BreakingBlock : MonoBehaviour
 {
     [SerializeField]
+    private GameObject regularMesh;
+    [SerializeField]
     private GameObject breakingMesh;
+
+    private const float DELAY = 5.0f;
 
     private readonly List<GameObject> allBreakablePieces = new();
 
@@ -19,20 +23,57 @@ public class BreakingBlock : MonoBehaviour
             allBreakablePieces.Add(breakablePiece);
         }
 
-        StartCoroutine(Delay(count));
+        ToggleBreakable(false);
+        BreakApart();
     }
-
-    
     private void Update()
     {
         
     }
-
-    private IEnumerator Delay(int count)
+    private void OnCollisionEnter(Collision collision)
     {
-        yield return new WaitForSeconds(5.0f);
+        if (collision.gameObject.CompareTag("Player") && collision.gameObject.GetComponent<Animator>().GetBool("IsRolling"))
+        {
+            //BreakApart();
+        }
+    }
+    private void ToggleBreakable(bool activated)
+    {
+        if (regularMesh.TryGetComponent<MeshRenderer>(out var meshRenderer))
+        {
+            meshRenderer.enabled = !activated;
+        }
+            
+        if (regularMesh.TryGetComponent<BoxCollider>(out var boxCollider))
+        {
+            boxCollider.enabled = !activated;
+        }
 
-        for (int i = 0; i < count; i++)
+        for (int i = 0; i < allBreakablePieces.Count; i++)
+        {
+            GameObject breakablePiece = allBreakablePieces[i];
+
+            if (breakablePiece.TryGetComponent<MeshRenderer>(out var breakableMeshRenderer))
+            {
+                breakableMeshRenderer.enabled = activated;
+            }
+
+            if (breakablePiece.TryGetComponent<MeshCollider>(out var meshCollider))
+            {
+                meshCollider.enabled = activated;
+            }
+        }
+    }
+    private void BreakApart()
+    {
+        ToggleBreakable(true);
+        //StartCoroutine(DestroyDelay());
+    }
+    private IEnumerator DestroyDelay()
+    {
+        yield return new WaitForSeconds(DELAY);
+
+        for (int i = 0; i < allBreakablePieces.Count; i++)
         {
             Destroy(allBreakablePieces[i]);
         }
